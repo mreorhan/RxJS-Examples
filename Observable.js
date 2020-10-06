d886de5e762ab34644831f66e2ce7ed164423018
Emre ORHAN <emre.orhan@argela.com.tr>
2020-10-06 16:15:13 +0300

    (~) Update observable

--- Observable.js
+++ Observable.js
@@ -1,31 +1,34 @@
-const { range, of ,Observable } = require('rxjs');
-const { map, filter } = require('rxjs/operators');
+const { Observable, from } = require('rxjs');
 
-const print = val => console.log(JSON.stringify(val))
-
-print(of(1, 2, 3));
-
-range(1, 20)
-.pipe(
-  filter(x => x % 2 === 1),
-  map(x => x + x)
-)
-.subscribe(x => console.log(x));
-
-const observable = new Observable(subscriber => {
+  //producer
+  const observable = new Observable(subscriber => {
     subscriber.next(1);
     subscriber.next(2);
     subscriber.next(3);
+    //like multiple return
     setTimeout(() => {
       subscriber.next(4);
       subscriber.complete();
     }, 1000);
+    if(1==0)
+    subscriber.error("error")
   });
    
   console.log('just before subscribe');
+  //consumer
   observable.subscribe({
     next(x) { console.log('got value ' + x); },
     error(err) { console.error('something wrong occurred: ' + err); },
     complete() { console.log('done'); }
   });
   console.log('just after subscribe');
+  //OR
+  observable.subscribe(
+    x=>console.log(x),
+    err=>console.log(err),
+    ()=>console.log("done")
+  )
+
+
+  const observable2=from([1,2,3,4,5])
+  observable2.subscribe(x=>console.log(x))
